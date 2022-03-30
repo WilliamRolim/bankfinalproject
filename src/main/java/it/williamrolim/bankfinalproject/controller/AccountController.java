575165c4ceec90b4191ddf7ef91f7e8e2b3a21b4
William R -global <williamrolimads@gmail.com>
2022-03-30 17:19:48 -0300

    12 - changes post/put - Account controller - Simple changes

--- src/main/java/it/williamrolim/bankfinalproject/controller/AccountController.java
+++ src/main/java/it/williamrolim/bankfinalproject/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,7 +76,7 @@ public class AccountController {
     }
     
     @ApiOperation(value= "Atualizando a conta pelo ID", notes = "Endpoint destinado o id pelo registro", response = Account.class)
-    @PostMapping("/update/{accountId}")
+    @PutMapping("/update/{accountId}")
     public ResponseEntity<Account> updateAccount(@RequestBody final AccountRequestDTO accountRequestDTO,
                                                @PathVariable final Integer accountId) {
     	Account account = accountService.updateAccount(accountId, accountRequestDTO);
